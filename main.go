b2de82a3b81f981435be1bf79887d8c99bb2f8c2
Kurt Ball <kurtisdball@hotmail.com>
2024-11-10 11:24:46 -0700

    first

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"utils"
+	// "strings"
+	// "text/scanner"
+)
+
+func scanLasFile(lasFile string) {
+	// Check the file extension
+	if filepath.Ext(lasFile) != ".las" {
+		fmt.Println("Invalid file extension")
+		return
+	}
+
+	file, err := os.Open(lasFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	target := ""
+
+	for scanner.Scan() {
+		// fmt.Printf("Scanning target: %s\n", target)
+		line := scanner.Text()
+
+		if strings.Contains(line, "~V") {
+			// fmt.Println("Populate version information")
+			// populate the version information struct skip to the next line
+			target = "VersionInformation"
+			continue
+		}
+
+		if strings.Contains(line, "~W") {
+			// fmt.Println("Populate version information")
+			target = "WellInformation"
+			continue
+		}
+
+		if strings.Contains(line, "~C") {
+			// fmt.Println("Populate curve information")
+			target = "CurveInformation"
+			continue
+		}
+
+		if strings.Contains(line, "~P") {
+			// fmt.Println("Populate parameter information")
+			target = "ParameterInformation"
+			// populate the parameter information struct skip to the next line
+			continue
+		}
+
+		if strings.Contains(line, "~O") {
+			// fmt.Println("Populate other information")
+			target = "OtherInformation"
+			// populate the other information struct skip to the next line
+			continue
+		}
+
+		if strings.Contains(line, "~A") {
+			// fmt.Println("Populate well information")
+			target = "DepthData"
+			// populate the well information struct skip to the next line
+			continue
+		}
+
+		utils.parseData(line, target)
+
+	}
+
+	// fmt.Printf("LasData: %s\n", lasData)
+}
+
+func main() {
+	// setup server
+
+	scanLasFile("mockData/test2.las")
+	fmt.Printf("LasData: %s\n", lasData)
+}
---
+++ utils/utils.go
@@ -0,0 +1,172 @@
+package main
+
+type EmptyStruct struct{}
+
+type StandardInformation struct {
+	Fields map[string]Data
+}
+
+type Data struct {
+	name        string
+	value       interface{}
+	description string
+	unit        string
+}
+
+type VersionInformation struct {
+	Fields map[string]Data
+}
+
+type WellInformation struct {
+	Fields map[string]Data
+}
+
+type CurveInformation struct {
+	Fields     map[string]Data
+	CurveOrder []string // Order of curve names for mapping data lines in ~A section
+}
+
+type Depth map[string]float64
+
+type AsciiLog struct {
+	Fields []map[string]string // List of data rows, each row is a map of field values
+}
+
+var lasData = struct {
+	VersionInformation   VersionInformation
+	WellInformation      WellInformation
+	CurveInformation     CurveInformation
+	otherInformation     EmptyStruct
+	parameterInformation EmptyStruct
+	WellData             AsciiLog
+}{
+	VersionInformation: VersionInformation{
+		make(map[string]Data),
+	},
+	WellInformation: WellInformation{
+		make(map[string]Data),
+	},
+	CurveInformation: CurveInformation{
+		Fields:     make(map[string]Data),
+		CurveOrder: []string{},
+	},
+	WellData: AsciiLog{
+		[]map[string]string{},
+	},
+}
+
+func handleStandardInformation(line string) (Data, error) {
+	if line == "" {
+		return Data{}, errors.New("Empty line")
+	}
+
+	index := strings.Index(line, ".")
+	descriptionIndex := strings.LastIndex(line, ":")
+
+	if index == -1 || descriptionIndex == -1 {
+		return Data{}, errors.New("Invalid line format")
+	}
+
+	key := strings.TrimSpace(line[:index])
+	unit := strings.TrimSpace(line[index+1 : index+2])
+	data := strings.TrimSpace(line[index+1 : descriptionIndex])
+	description := strings.TrimSpace(line[descriptionIndex+1:])
+
+	newEntry := Data{
+		name:        key,
+		value:       data,
+		description: description,
+		unit:        unit,
+	}
+
+	return newEntry, nil
+}
+
+func handleCurveInformation(line string) Data {
+	substrings := []string{"~Version", "~WELL", "#", "~Curve", "~Parameter", "~Other", "~A"}
+	for _, substring := range substrings {
+		if strings.Contains(line, substring) {
+			return Data{}
+		}
+	}
+
+	index := strings.Index(line, ".")
+	descriptionIndex := strings.LastIndex(line, ":")
+
+	if index == -1 {
+		return Data{}
+	}
+
+	// if no description index is found, set it to the end of the line
+	newEntry := Data{
+		name:        strings.TrimSpace(line[:index]),
+		value:       strings.TrimSpace(line[index+1:]),
+		description: strings.TrimSpace(line[descriptionIndex+1:]),
+		unit:        strings.TrimSpace(line[index+1 : index+2]),
+	}
+
+	return newEntry
+}
+
+func handleData(line string) map[string]string {
+	// Split the line by whitespace to get each data value
+	values := strings.Fields(line)
+	if len(values) != len(lasData.CurveInformation.CurveOrder) {
+		fmt.Println("Data line does not match curve information")
+		return nil
+	}
+
+	// Map each value to the curve name and add it as a new entry in wellData.Fields
+	dataEntry := make(map[string]string)
+	for i, value := range values {
+		curveName := lasData.CurveInformation.CurveOrder[i]
+		dataEntry[curveName] = value
+	}
+	lasData.WellData.Fields = append(lasData.WellData.Fields, dataEntry)
+	// fmt.Printf("Added data entry: %+v\n", dataEntry)
+
+	return dataEntry
+}
+
+func parseData(line string, target string) []string {
+	substrings := []string{"~Version", "~WELL", "#", "~Curve", "~Parameter", "~Other", "~A"}
+	for _, substring := range substrings {
+		if strings.Contains(line, substring) {
+			return nil
+		}
+	}
+
+	// Populate the appropriate field based on the target
+	switch target {
+	case "VersionInformation":
+		newEntry, errors := handleStandardInformation(line)
+		if errors != nil {
+			fmt.Println(errors)
+			return nil
+		}
+
+		lasData.VersionInformation.Fields[newEntry.name] = newEntry
+		// fmt.Printf("Added entry for %s - Key: %s, Data: %v, Description: %s\n", target, newEntry.name, newEntry.value, newEntry.description)
+	case "WellInformation":
+		newEntry, errors := handleStandardInformation(line)
+		if errors != nil {
+			fmt.Println(errors)
+			return nil
+		}
+
+		lasData.WellInformation.Fields[newEntry.name] = newEntry
+		// fmt.Printf("Added entry for %s - Key: %s, Data: %v, Description: %s\n", target, newEntry.name, newEntry.value, newEntry.description)
+	case "CurveInformation":
+		newEntry := handleCurveInformation(line)
+		lasData.CurveInformation.Fields[newEntry.name] = newEntry
+		lasData.CurveInformation.CurveOrder = append(lasData.CurveInformation.CurveOrder, newEntry.name)
+	case "DepthData":
+		data := handleData(line)
+		lasData.WellData.Fields = append(lasData.WellData.Fields, data)
+		// fmt.Printf("Added data entry: %+v\n", data)
+	default:
+		fmt.Printf("Unknown target: %s\n", target)
+	}
+
+	return nil
+}
